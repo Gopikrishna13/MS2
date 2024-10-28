8002a303f15f5344070d5c118df71b46c590625c
Gopikrishna13 <108319104+Gopikrishna13@users.noreply.github.com>
2024-10-28 22:58:52 +0530

    Update BikeInventory.cs

--- MS2/MotorBikeRental/Database/Entities/BikeInventory.cs
+++ MS2/MotorBikeRental/Database/Entities/BikeInventory.cs
@@ -6,10 +6,12 @@ namespace MotorBikeRental.Database.Entities
 
 public class BikeInventory
 {
-   public int BikeId{get;set;}
+    public int BikeId{get;set;}
    public string Brand {get;set;}
    public string Model{get;set;}
    public string RegistrationNumber{get;set;}
+   public DateTime RentedDate{get;set;}
+   public DateTime ReturnDate{get;set;}
    public string Status{get;set;}
 }
 
